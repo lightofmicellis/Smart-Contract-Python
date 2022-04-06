c64e7cb89442cbb7bb584aaa399c71b981eb5934
lightofmicellis <lightofmicellis@gmail.com>
2022-04-07 00:14:25 +0200

    Appending the Block to the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
-# Importing the Various Modules and Libraries 
+# Importing the Various Modules and Libraries #
+from ast import Return
 import sys
 import hashlib
 import json
@@ -8,7 +9,7 @@ from flask import Flask, jsonify, request
 import requests
 from urllib.parse import urlparse
 
-# Declaring the Class in Python
+# Declaring the Class in Python #
 
 class Blockchain(object):
     difficulty_target = "0000"
@@ -33,7 +34,7 @@ class Blockchain(object):
 # The preceding creates a class name blockchain with 
 # two method: hash_block and __init__
 
-# Finding the Nonce
+# Finding the Nonce #
 
 # Use PoW to find the nonce for the current block
 
@@ -62,11 +63,42 @@ def proof_of_work(self, index, hash_of_previous_block, transactions):
 
 def valid_proof(self, index, hash_of_previous_block,
     transactions, nonce):
-     # create a string containing the hash of the previous
-     # block and the block content, including the nonce
+    # create a string containing the hash of the previous
+    # block and the block content, including the nonce
     content = f'{index} {hash_of_previous_block} {transactions} {nonce}'.encode()
     # hash using sha256
     content_hash = hashlib.sha256(content).hexdigest()
     # check if the hash meets the difficulty target 
     return content_hash[:len(self.difficulty_target)] == self.difficult_target
-    
+
+# Appending the Block to the Blockchain #
+
+
+# Once the nonce for a block has been found, you can now write the method to 
+# append the block to the existing blockchain. This is the function of the 
+# append_block() method :
+
+# Write a new block and append it to the blockchain
+
+def append_block(self, nonce, hash_of_previous_block):
+    block = {
+        'index': len(self.chain),
+        'timestamp': time(),
+        'transactions': self.current_transactions,
+        'hash_previous_block': hash_of_previous_block
+    }
+    # Reset the current list of transactions
+    self.current_transactions = []
+    # Add the new block to the blockchain
+    self.chain.append(block)
+    return block
+
+
+# When the block is added to the blockchain, the current timestamp is also added 
+# to the block.
+
+
+
+
+
+
